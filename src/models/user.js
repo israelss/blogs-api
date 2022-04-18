e4ec569b9908094196aa4e97004a6b297c3e4be1
Israel Sant'Anna <israel.santanna@gmail.com>
2022-04-18 20:04:08 -0300

    feat: add User model and migration

---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+const attributes = (DataTypes) => ({
+  displayName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+const User = (sequelize, DataTypes) => sequelize
+  .define('User', attributes(DataTypes));
+
+module.exports = User;
