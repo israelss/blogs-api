13661e2d1df6ccaa5c4b1bb869599a5ed7dd457d
Israel Sant'Anna <israel.santanna@gmail.com>
2022-04-18 20:06:14 -0300

    feat: add user controller
     - create

---
+++ src/controllers/user/index.js
@@ -0,0 +1,11 @@
+const { success } = require('../../helpers/status_codes');
+const userServices = require('../../services/user');
+
+const create = async (req, res) => {
+  const token = await userServices.create(req.body);
+  return res.status(success.CREATED).json(token);
+};
+
+module.exports = {
+  create,
+};
