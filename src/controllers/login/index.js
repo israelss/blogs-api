0ca64a77ebba5ba67f70b64f7b3dc7543eec24ef
Israel Sant'Anna <israel.santanna@gmail.com>
2022-04-19 15:56:59 -0300

    feat: add login controller
     - login

---
+++ src/controllers/login/index.js
@@ -0,0 +1,12 @@
+const { success, clientError } = require('../../helpers/status_codes');
+const loginServices = require('../../services/login');
+
+const login = async (req, res) => {
+  const token = await loginServices.login(req.body);
+  if (!token) return res.status(clientError.BAD_REQUEST).json({ message: 'Invalid fields' });
+  return res.status(success.OK).json(token);
+};
+
+module.exports = {
+  login,
+};
