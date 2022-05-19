6322a0633e6cbf8d521271e2bf087c636a1661d5
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-19 17:43:25 -0300

    chore: adequate project to Trybe rules
    So I can publish on my personal GitHub!

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
